d07437d27c959a1f307b9786b531cb64cf4f2faa
GojoDevloper <154049785+GojoDevloper@users.noreply.github.com>
2024-01-15 15:31:28 +0530

    Update upload.py

--- shivu/modules/upload.py
+++ shivu/modules/upload.py
@@ -132,7 +132,7 @@ async def update(update: Update, context: CallbackContext) -> None:
         if args[1] in ['name', 'anime']:
             new_value = args[2].replace('-', ' ').title()
         elif args[1] == 'rarity':
-            rarity_map = {1: "⚪ Common", 2: "🟣 Rare", 3: "🟡 Legendary", 4: "🟢 Medium", 5: "💮 Exclusive", 6: "👶 Chibi", 7: (🔮 Limited Edition)}
+            rarity_map = {1: "⚪ Common", 2: "🟣 Rare", 3: "🟡 Legendary", 4: "🟢 Medium", 5: "💮 Exclusive", 6: "👶 Chibi", 7: "🔮 Limited Edition"}
             try:
                 new_value = rarity_map[int(args[2])]
             except KeyError:
